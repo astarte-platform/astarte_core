c9b2efb712d9a5da16b008659a264c8c117e69dd
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-18 16:51:03 +0200

    cql_utils: add interface_descriptor_statement function

--- lib/astarte_core/cql_utils.ex
+++ lib/astarte_core/cql_utils.ex
@@ -1,11 +1,21 @@
 defmodule Astarte.Core.CQLUtils do
 
+  @interface_descriptor_statement """
+    SELECT name, major_version, minor_version, type, quality, flags
+    FROM interfaces
+    WHERE name=:name AND major_version=:major_version
+  """
+
   def interface_name_to_table_name(interface_name, major_version) do
     String.replace(String.downcase(interface_name), ".", "_") <> "_v" <> Integer.to_string(major_version)
   end
 
   def endpoint_to_db_column_name(endpoint_name) do
     List.last(String.split(String.downcase(endpoint_name), "/"))
+  end
+
+  def interface_descriptor_statement() do
+    @interface_descriptor_statement
   end
 
   def mapping_value_type_to_db_type(value_type) do
