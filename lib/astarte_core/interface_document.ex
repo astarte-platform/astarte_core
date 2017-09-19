91f4fd9d90152f5e71a317936c95880b67193aca
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-19 18:23:13 +0200

    interface_document: make from_json return {:ok, res} or :error
    Also, add some docs

--- lib/astarte_core/interface_document.ex
+++ lib/astarte_core/interface_document.ex
@@ -11,46 +11,56 @@ defmodule Astarte.Core.InterfaceDocument do
     true
   end
 
+  @doc """
+  Creates an `%InterfaceDocument{}` starting from its JSON description.
+
+  Returns `{:ok, %InterfaceDocument{}}` on success and `:error` on failure.
+  """
   def from_json(json_doc) do
-    {:ok, interface_object} = Poison.decode(json_doc)
+    case Poison.decode(json_doc) do
+      {:error, _} -> :error
 
-    %{"interface_name" => name,
-      "version_major" => major_version,
-      "version_minor" => minor_version,
-      "type" => type} = interface_object
+      {:ok, interface_object} ->
 
-    descriptor = %Astarte.Core.InterfaceDescriptor {
-      name: name,
-      major_version: major_version,
-      minor_version: minor_version,
-      type: Astarte.Core.Interface.Type.from_string(type),
-      ownership: Astarte.Core.Interface.Ownership.from_string(interface_object["ownership"] || interface_object["quality"]),
-      aggregation: Astarte.Core.Interface.Aggregation.from_string((if interface_object["aggregate"], do: "object", else: nil)
-                                                                  || Map.get(interface_object, "aggregation", "individual")),
-      has_metadata: Map.get(interface_object, "has_metadata", false),
-      explicit_timestamp: Map.get(interface_object, "explicit_timestamp", false)
-    }
+        %{"interface_name" => name,
+          "version_major" => major_version,
+          "version_minor" => minor_version,
+          "type" => type
+        } = interface_object
 
-    maps = for mapping <- interface_object["mappings"] do
-      %Astarte.Core.Mapping {
-        endpoint: mapping["endpoint"] || mapping["path"],
-        value_type: Astarte.Core.Mapping.ValueType.from_string(mapping["type"]),
-        reliability: Astarte.Core.Mapping.Reliability.from_string(Map.get(mapping, "reliability", "unreliable")),
-        retention: Astarte.Core.Mapping.Retention.from_string(Map.get(mapping, "retention", "discard")),
-        expiry: Map.get(mapping, "expiry", 0),
-        allow_unset: Map.get(mapping, "allow_unset", false)
-      }
-    end
+        descriptor = %Astarte.Core.InterfaceDescriptor {
+          name: name,
+          major_version: major_version,
+          minor_version: minor_version,
+          type: Astarte.Core.Interface.Type.from_string(type),
+          ownership: Astarte.Core.Interface.Ownership.from_string(interface_object["ownership"] || interface_object["quality"]),
+          aggregation: Astarte.Core.Interface.Aggregation.from_string((if interface_object["aggregate"], do: "object", else: nil)
+                                                                      || Map.get(interface_object, "aggregation", "individual")),
+          has_metadata: Map.get(interface_object, "has_metadata", false),
+          explicit_timestamp: Map.get(interface_object, "explicit_timestamp", false)
+        }
 
-    if (check_mappings(maps) and Astarte.Core.InterfaceDescriptor.is_valid?(descriptor)) do
-      %Astarte.Core.InterfaceDocument {
-        descriptor: descriptor,
-        mappings: maps,
-        source: json_doc
-      }
-    else
-      nil
-    end
+        maps = for mapping <- interface_object["mappings"] do
+          %Astarte.Core.Mapping {
+            endpoint: mapping["endpoint"] || mapping["path"],
+            value_type: Astarte.Core.Mapping.ValueType.from_string(mapping["type"]),
+            reliability: Astarte.Core.Mapping.Reliability.from_string(Map.get(mapping, "reliability", "unreliable")),
+            retention: Astarte.Core.Mapping.Retention.from_string(Map.get(mapping, "retention", "discard")),
+            expiry: Map.get(mapping, "expiry", 0),
+            allow_unset: Map.get(mapping, "allow_unset", false)
+          }
+        end
+
+        if (check_mappings(maps) and Astarte.Core.InterfaceDescriptor.is_valid?(descriptor)) do
+          doc = %Astarte.Core.InterfaceDocument {
+            descriptor: descriptor,
+            mappings: maps,
+            source: json_doc
+          }
+          {:ok, doc}
+        else
+          :error
+        end
+      end
   end
-
 end
