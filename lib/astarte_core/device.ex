fc251fa8b8b7f448060c258eadaf3025ac0b297a
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-04-05 16:17:14 +0200

    device: add decode_device_id function

--- lib/astarte_core/device.ex
+++ lib/astarte_core/device.ex
@@ -2,4 +2,27 @@ defmodule Astarte.Core.Device do
   @moduledoc """
   Utility functions to deal with Astarte devices
   """
+
+  @doc """
+  Decodes a Base64 url encoded device id and returns it as a 128-bit binary (usable as uuid).
+
+  By default, it will fail with `{:error, :extended_id_not_allowed}` if the size of the encoded device_id is > 128 bit.
+  You can pass `allow_extended_id: true` as second argument to allow longer device ids (the returned binary will still be 128 bit long, but the function will not return an error and will instead drop the extended id).
+
+  Returns `{:ok, device_id}` or `{:error, reason}`.
+  """
+  def decode_device_id(encoded_device_id, opts \\ []) when is_binary(encoded_device_id) and is_list(opts) do
+    allow_extended = Keyword.get(opts, :allow_extended_id, false)
+    with {:ok, decoded} <- Base.url_decode64(encoded_device_id, padding: false),
+         <<device_id::binary-size(16), extended_id::binary>> <- decoded do
+      if not allow_extended and byte_size(extended_id) > 0 do
+        {:error, :extended_id_not_allowed}
+      else
+        {:ok, device_id}
+      end
+    else
+      _ ->
+        {:error, :invalid_device_id}
+    end
+  end
 end
