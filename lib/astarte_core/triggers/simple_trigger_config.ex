8c1188542a3d830c62218eef6a640904c9479342
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-04-16 17:36:38 +0200

    simple_trigger_config: add from_tagged_simple_trigger function

--- lib/astarte_core/triggers/simple_trigger_config.ex
+++ lib/astarte_core/triggers/simple_trigger_config.ex
@@ -1,6 +1,6 @@
 defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
   @moduledoc """
-  This module handles the functions for creating a `SimpleTriggerConfig` and converting it to a `TaggedSimpleTrigger`.
+  This module handles the functions for creating a `SimpleTriggerConfig` and converting it to and from a `TaggedSimpleTrigger`.
   """
   use Astarte.Core.Triggers.SimpleTriggersProtobuf
   use Ecto.Schema
@@ -49,6 +49,14 @@ defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
     "path_removed" => :PATH_REMOVED,
     "value_stored" => :VALUE_STORED
   }
+  @data_trigger_condition_to_string %{
+    :INCOMING_DATA => "incoming_data",
+    :VALUE_CHANGE => "value_change",
+    :VALUE_CHANGE_APPLIED => "value_change_applied",
+    :PATH_CREATED => "path_created",
+    :PATH_REMOVED => "path_removed",
+    :VALUE_STORED => "value_stored"
+  }
   @data_trigger_operator_to_atom %{
     "*" => :ANY,
     "==" => :EQUAL_TO,
@@ -59,6 +67,17 @@ defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
     "<=" => :LESS_OR_EQUAL_TO,
     "contains" => :CONTAINS,
     "not_contains" => :NOT_CONTAINS
+  }
+  @data_trigger_operator_to_string %{
+    :ANY => "*",
+    :EQUAL_TO => "==",
+    :NOT_EQUAL_TO => "!=",
+    :GREATER_THAN => ">",
+    :GREATER_OR_EQUAL_TO => ">=",
+    :LESS_THAN => "<",
+    :LESS_OR_EQUAL_TO => "<=",
+    :CONTAINS => "contains",
+    :NOT_CONTAINS => "not_contains"
   }
   @data_trigger_any_match_operator "*"
 
@@ -72,6 +91,12 @@ defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
     "device_disconnected" => :DEVICE_DISCONNECTED,
     "device_empty_cache_received" => :DEVICE_EMPTY_CACHE_RECEIVED,
     "device_error" => :DEVICE_ERROR
+  }
+  @device_trigger_condition_to_string %{
+    :DEVICE_CONNECTED => "device_connected",
+    :DEVICE_DISCONNECTED => "device_disconnected",
+    :DEVICE_EMPTY_CACHE_RECEIVED => "device_empty_cache_received",
+    :DEVICE_ERROR => "device_error"
   }
 
   @allowed_trigger_types [
@@ -133,6 +158,21 @@ defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
     simple_trigger_config
     |> put_device_trigger_atoms()
     |> create_tagged_device_trigger()
+  end
+
+  def from_tagged_simple_trigger(%TaggedSimpleTrigger{} = tagged_simple_trigger) do
+    %TaggedSimpleTrigger{
+      object_id: object_id,
+      simple_trigger_container: simple_trigger_container
+    } = tagged_simple_trigger
+
+    case simple_trigger_container.simple_trigger do
+      {:data_trigger, %DataTrigger{} = data_trigger} ->
+        from_data_trigger(data_trigger)
+
+      {:device_trigger, %DeviceTrigger{} = device_trigger} ->
+        from_device_trigger(device_trigger, object_id)
+    end
   end
 
   defp validate_match_parameters(%Ecto.Changeset{} = changeset) do
@@ -223,6 +263,54 @@ defmodule Astarte.Core.Triggers.SimpleTriggerConfig do
       simple_trigger_container: %SimpleTriggerContainer{
         simple_trigger: {:device_trigger, device_trigger}
       }
+    }
+  end
+
+  defp from_data_trigger(%DataTrigger{} = data_trigger) do
+    %DataTrigger{
+      data_trigger_type: data_trigger_type,
+      interface_id: _interface_id,
+      value_match_operator: value_match_operator,
+      match_path: match_path,
+      known_value: known_value
+    } = data_trigger
+
+    condition = Map.fetch!(@data_trigger_condition_to_string, data_trigger_type)
+    value_match_operator_string = Map.fetch!(@data_trigger_operator_to_string, value_match_operator)
+    decoded_known_value =
+      if known_value do
+        Bson.decode(known_value)
+        |> Map.get(:v)
+      else
+        nil
+      end
+
+    # TODO: interface_name and interface_major can't be deducted from interface_id,
+    # leaving them nil waiting for an API to retrieve them
+    %SimpleTriggerConfig{
+      type: "data_trigger",
+      on: condition,
+      interface_name: nil,
+      interface_major: nil,
+      value_match_operator: value_match_operator_string,
+      match_path: match_path,
+      known_value: decoded_known_value
+    }
+  end
+
+  defp from_device_trigger(%DeviceTrigger{} = device_trigger, device_id) do
+    %DeviceTrigger{
+      device_event_type: device_event_type
+    } = device_trigger
+
+    encoded_device_id = Device.encode_device_id(device_id)
+
+    condition = Map.fetch!(@device_trigger_condition_to_string, device_event_type)
+
+    %SimpleTriggerConfig{
+      type: "device_trigger",
+      on: condition,
+      device_id: encoded_device_id
     }
   end
 end
