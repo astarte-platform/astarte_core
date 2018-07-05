e6f4ffe6ed938b761f8e3b9a1cbce2fe8ce9fb64
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-07-05 17:58:29 +0200

    retention: make it an Ecto Type

--- lib/astarte_core/mapping/retention.ex
+++ lib/astarte_core/mapping/retention.ex
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 # This file is part of Astarte.
 # Astarte is free software: you can redistribute it and/or modify
@@ -17,31 +17,77 @@
 #
 
 defmodule Astarte.Core.Mapping.Retention do
+  @behaviour Ecto.Type
+
   @mapping_retention_discard 1
   @mapping_retention_volatile 2
   @mapping_retention_stored 3
+  @valid_atoms [
+    :discard,
+    :volatile,
+    :stored
+  ]
 
-  def to_int(retention) do
-    case retention do
-      :discard -> @mapping_retention_discard
-      :volatile -> @mapping_retention_volatile
-      :stored -> @mapping_retention_stored
+  @impl true
+  def type, do: :integer
+
+  @impl true
+  def cast(nil), do: {:ok, nil}
+
+  def cast(atom) when is_atom(atom) do
+    if Enum.member?(@valid_atoms, atom) do
+      {:ok, atom}
+    else
+      :error
     end
   end
 
-  def from_int(retention_int) do
+  def cast(string) when is_binary(string) do
+    case string do
+      "discard" -> {:ok, :discard}
+      "volatile" -> {:ok, :volatile}
+      "stored" -> {:ok, :stored}
+      _ -> :error
+    end
+  end
+
+  def cast(_), do: :error
+
+  @impl true
+  def dump(retention) when is_atom(retention) do
+    case retention do
+      :discard -> {:ok, @mapping_retention_discard}
+      :volatile -> {:ok, @mapping_retention_volatile}
+      :stored -> {:ok, @mapping_retention_stored}
+      _ -> :error
+    end
+  end
+
+  def to_int(retention) when is_atom(retention) do
+    case dump(retention) do
+      {:ok, int} -> int
+      :error -> raise ArgumentError, message: "#{inspect(retention)} is not a valid retention"
+    end
+  end
+
+  @impl true
+  def load(retention_int) when is_integer(retention_int) do
     case retention_int do
-      @mapping_retention_discard -> :discard
-      @mapping_retention_volatile -> :volatile
-      @mapping_retention_stored -> :stored
+      @mapping_retention_discard -> {:ok, :discard}
+      @mapping_retention_volatile -> {:ok, :volatile}
+      @mapping_retention_stored -> {:ok, :stored}
+      _ -> :error
     end
   end
 
-  def from_string(retention) do
-    case retention do
-      "discard" -> :discard
-      "volatile" -> :volatile
-      "stored" -> :stored
+  def from_int(retention_int) when is_integer(retention_int) do
+    case load(retention_int) do
+      {:ok, retention} ->
+        retention
+
+      :error ->
+        raise ArgumentError,
+          message: "#{retention_int} is not a valid retention int representation"
     end
   end
 end
