5269f82ff59b8b97e83c2ecd888d2fbe21cf4d55
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-07-11 15:52:30 +0200

    interface_descriptor: remove validation functions
    Validation is now done through the Interface changeset

--- lib/astarte_core/interface_descriptor.ex
+++ lib/astarte_core/interface_descriptor.ex
@@ -33,64 +33,6 @@ defmodule Astarte.Core.InterfaceDescriptor do
             storage: nil,
             storage_type: nil
 
-  def validate(interface_descriptor) do
-    cond do
-      interface_descriptor == nil ->
-        {:error, :null_interface_descriptor}
-
-      interface_descriptor == "" ->
-        {:error, :not_an_interface_descriptor}
-
-      interface_descriptor == [] ->
-        {:error, :not_an_interface_descriptor}
-
-      String.length(interface_descriptor.name) > 128 ->
-        {:error, :too_long_interface_name}
-
-      String.match?(interface_descriptor.name, interface_name_regex()) == false ->
-        {:error, :invalid_interface_name}
-
-      interface_descriptor.major_version < 0 ->
-        {:error, :invalid_major_version}
-
-      interface_descriptor.major_version < 0 ->
-        {:error, :invalid_minor_version}
-
-      interface_descriptor.major_version == 0 and interface_descriptor.minor_version == 0 ->
-        {:error, :invalid_minor_version}
-
-      (interface_descriptor.type == :properties or interface_descriptor.type == :datastream) ==
-          false ->
-        {:error, :invalid_interface_type}
-
-      (interface_descriptor.ownership == :device or interface_descriptor.ownership == :server) ==
-          false ->
-        {:error, :invalid_interface_ownership}
-
-      (interface_descriptor.aggregation == :individual or
-         interface_descriptor.aggregation == :object) == false ->
-        {:error, :invalid_interface_aggregation}
-
-      interface_descriptor.type != :datastream and interface_descriptor.explicit_timestamp ->
-        {:error, :explicit_timestamp_not_allowed}
-
-      (interface_descriptor.aggregation != :individual or interface_descriptor.type != :datastream) and
-          interface_descriptor.has_metadata ->
-        {:error, :metadata_not_allowed}
-
-      true ->
-        :ok
-    end
-  end
-
-  def is_valid?(interface_descriptor) do
-    validate(interface_descriptor) == :ok
-  end
-
-  def interface_name_regex do
-    ~r/^[a-zA-Z]+(\.[a-zA-Z0-9]+)*$/
-  end
-
   @doc """
   Deserializes an `%InterfaceDescriptor{}` from `db_result`.
   `db_result` can be a keyword list or a map.
