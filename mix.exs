2ad303adbc0158b49aef4933e252e77af8fa0327
Davide Bettio <davide.bettio@ispirata.com>
2019-04-24 17:17:37 +0200

    Update required Elixir version to 1.8.1 and OTP 21.3
    astarte_core will be tested against Elixir 1.8.1 and OTP 21.3.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.Core.Mixfile do
     [
       app: :astarte_core,
       version: "0.11.0-dev",
-      elixir: "~> 1.4",
+      elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
