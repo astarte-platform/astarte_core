f86a0cf77911171cc225e3e47b5836cf8079330c
Davide Bettio <davide.bettio@secomind.com>
2023-06-09 17:24:57 +0200

    Prepare next dev cycle (v1.2.0-dev)
    Signed-off-by: Davide Bettio <davide.bettio@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.Core.Mixfile do
   def project do
     [
       app: :astarte_core,
-      version: "1.1.0-rc.0",
+      version: "1.2.0-dev",
       elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
