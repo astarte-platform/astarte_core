c5ffe940350a311ac81f77a78422860992d4313e
Francesco Noacco <francesco.noacco@secomind.com>
2025-03-18 11:31:20 +0100

    chore: update version and changelog
    restore correct version after forward port
    
    keep the Unreleased tag in the changelog
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.Core.Mixfile do
   def project do
     [
       app: :astarte_core,
-      version: "1.2.0",
+      version: "1.3.0-dev",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
