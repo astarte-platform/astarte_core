dbc315f937492bac7b08d05d709c0617bb890f54
frossq <francesco.asquini@secomind.com>
2026-08-19 15:25:56 +0200

    chore: prepare release 1.4.0-rc-5
    Signed-off-by: frossq <francesco.asquini@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.Core.Mixfile do
   def project do
     [
       app: :astarte_core,
-      version: "1.4.0-rc.4",
+      version: "1.4.0-rc.5",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
